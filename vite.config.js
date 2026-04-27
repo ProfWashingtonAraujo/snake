f297ad23ee6b1d1ed9902455e117ae063f5a2466
ProfWashingtonAraujo <washington.araujo@gmail.com>
2026-04-27 11:22:48 -0300

    chore: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/snake-premium-arcade/', // Change this to your repository name
+  base: '/snake/', // Set to repository name for GitHub Pages
 })
